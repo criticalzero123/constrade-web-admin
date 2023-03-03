c8ea51185ec5abc25b75c70adb1cc65965dbd177
criticalzero123 <jdcaramonte159@gmail.com>
2023-03-03 21:28:29 +0800

    Removing sign up

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./views/LandingPage/LandingPage";
 import SignIn from "./views/SignIn/SignIn";
-import SignUp from "./views/SignUp/SignUp";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 }
